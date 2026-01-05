aff2b31c17a39194fd73bf2f158549bc953bec3b
its-kevin228 <pekpelignimdoukevin@gmail.com>
2026-01-05 09:56:10 +0000

    initialise project

---
+++ ega-bank/src/test/java/com/ega/ega_bank/EgaBankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ega.ega_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaBankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
